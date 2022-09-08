b9d225faa50475f72cad05aff81b6c59641df0b2
Dmitrii Khramtsov <dmitrypkh@gmail.com>
2022-09-08 12:16:55 +0300

    code refactoring

--- time_tracker/time_tracker.py
+++ time_tracker/time_tracker.py
@@ -20,8 +20,7 @@ class TimeTracker:
         description - currently not implemented
         """
         """add new task"""
-        if len(self.session.query(Task).\
-                            filter(Task.name == name).all()) == 0:
+        if not self._task_exists(name):
             self.session.add(
                             Task(name = name,
                                 description = description)
@@ -37,9 +36,8 @@ class TimeTracker:
     def task_remove(self, name):
         # handle - not found and write test
         """remove task"""
-        if len(self.session.query(Task).\
-                            filter(Task.name == name).all()) == 1:
-            task_id = self.session.query(Task).filter(Task.name == name).one().id
+        if self._task_exists(name):
+            task_id = self._task_id(name)
             # to-do: wrap in transaction
             self.session.query(Task).filter(Task.id == task_id).delete()
             self.session.query(WorkBlock).filter(WorkBlock.task_id == task_id).delete()
@@ -55,17 +53,17 @@ class TimeTracker:
         return message what task is running already
         handle task not found
         """
-        if len(self.session.query(Task).filter(Task.name == name).all()) == 0:
+        if not self._task_exists(name):
             return f'Task {name} not found. Maybe task was incorrectly typed?'
         # if there is an active task - stop it first
         # if active task is the same - show message
         if self._any_active_task():
-            task_id = self.session.query(Task).filter(Task.name == name).one().id
+            task_id = self._task_id(name)
             if self.session.query(WorkBlock).filter(WorkBlock.finish_time == None).\
                                              one().task_id == task_id:
                 return f'Task {name} is already in progress'
             self.task_finish()
-        task_id = self.session.query(Task).filter(Task.name == name).one().id
+        task_id = self._task_id(name)
         self.session.add(
                         WorkBlock(task_id = task_id,
                                   start_time = int(time.time()))
@@ -107,10 +105,13 @@ class TimeTracker:
             tasks_output.append(f"{task.name}{' (in progress)' if task.id == active_task_id else ''}")
         return '\n'.join(tasks_output)
 
-    def tasks_stats(self):
+    def tasks_stats(self, task_name):
         """
         will be implemented in future versions
+        time spent on each task. Show only tasks that were active today.
+        Time is calculated for today (with respect to day_starts in _time_active_today())
         """
+        # today_work_blocks = self._work_blocks_today(self._task_id(task_name))
         return 'Feature is under development'
 
     def task_status(self):
@@ -141,11 +142,19 @@ class TimeTracker:
         """
         return amount of time spent on a given task today
         change when new day starts
-        in case you were working for example up to 1 A.M. it makes sence to track that activity 
-        as previous day
-        hours after midnight
         """
-        day_starts = 4
+        today_work_blocks = self._work_blocks_today(task_id)
+        today_work_time = 0
+        for today_work_block in today_work_blocks:
+            today_work_time += (today_work_block.finish_time - today_work_block.start_time)
+        return str(datetime.timedelta(seconds=today_work_time))
+
+    def _work_blocks_today(self, task_id):
+        """
+        in case you were working for example up to 1 A.M. it makes sence to track that activity 
+        as previous day hours after midnight
+        """
+        day_starts = 4 # hrs
         seconds_in_hour = 60 * 60
         seconds_in_day = 24 * seconds_in_hour
         epoch_time = int(time.time())
@@ -153,11 +162,14 @@ class TimeTracker:
         today_work_blocks = self.session.query(WorkBlock).\
                                          filter(WorkBlock.finish_time >= today_start_time).\
                                          filter(WorkBlock.task_id == task_id).all()
+        return today_work_blocks
 
-        today_wrok_time = 0
-        for today_work_block in today_work_blocks:
-            today_wrok_time += (today_work_block.finish_time - today_work_block.start_time)
-        return str(datetime.timedelta(seconds=today_wrok_time))
+    def _task_id(self, task_name):
+        return self.session.query(Task).filter(Task.name == task_name).one().id
+    
+    def _task_exists(self, task_name):
+        return len(self.session.query(Task).\
+                                filter(Task.name == task_name).all()) == 1
 
 # fix multiple null records
 # self.session.query(WorkBlock).filter(WorkBlock.finish_time == None).delete()
